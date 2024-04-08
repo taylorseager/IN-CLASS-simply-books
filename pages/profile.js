dd00f06436e0a5f9bcb792b2ea17a95d6d47931b
Taylor Seager <taylorseager718@gmail.com>
2024-04-07 22:49:57 -0500

    user component and page rendering

--- pages/profile.js
+++ pages/profile.js
@@ -1,16 +1,7 @@
-// /* eslint-disable react-hooks/exhaustive-deps */
-// import React, { useEffect, useState } from 'react';
-// import { Button } from 'react-bootstrap';
-// import Link from 'next/link';
-// import { useAuth } from '../../utils/context/authContext';
+import UserProfile from '../components/User';
 
-// function authorsPage() {
-//   const [authors, setAuthors] = useState([]);
-//   const { user } = useAuth();
-//   const getAllTheAuthors = () => {
-//     getAuthors(user.uid).then(setAuthors);
-//   };
-
-//   useEffect(() => {
-//     getAllTheAuthors();
-//   }, []);
+export default function Profile() {
+  return (
+    <UserProfile />
+  );
+}
